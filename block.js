7a17fb86a853d28d1479f34c7bcac1c4790dc7c7
Saurabh Shubham <saurabh.friday@gmail.com>
2018-02-03 12:08:43 +0530

    Added chain validity

--- block.js
+++ block.js
@@ -32,10 +32,29 @@ class Blockchain {
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid() {
+        for(let i = 1; i< this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i-1];
+
+            if(currentBlock.hash != currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if(currentBlock.previousHash != previousBlock.hash) {
+                return false;
+            }
+
+            return true;
+        }
+    }
 }
 
 let sscoin = new Blockchain();
 sscoin.addBlock(new Block(1, "01/01/2018", { amount: 8 }));
 sscoin.addBlock(new Block(2, "03/02/2018", { amount: 20 }));
 
-console.log(JSON.stringify(sscoin, null, 4));
+console.log("Is chain valid? " + sscoin.isChainValid());
+
+//console.log(JSON.stringify(sscoin, null, 4));
